0f7d4499d13c7296ce9da9178f29db033ab896a9
Nicola Costa <nicola.costa2002@proton.me>
2026-03-19 13:25:06 +0100

    implementazione metodi di testing precedentemente creati

--- src/test/java/it/nicolacosta/movie_app/builder/MovieBuilderTest.java
+++ src/test/java/it/nicolacosta/movie_app/builder/MovieBuilderTest.java
@@ -1,4 +1,45 @@
+package it.nicolacosta.movie_app.builder;
+
+import it.nicolacosta.movie_app.model.Movie;
+import it.nicolacosta.movie_app.model.TvSeries;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MovieBuilderTest {
-  
+
+
+    @Test
+    void build_shouldCreateMovieWithCorrectParams(){
+        Movie movie = new MovieBuilder()
+                .title("Interstellar")
+                .director("Nolan")
+                .year(2014)
+                .build();
+
+        assertEquals("Interstellar", movie.getTitle());
+        assertEquals("Nolan", movie.getDirector());
+        assertEquals(2014, movie.getYear());
+        assertNull(movie.getGenre());
+        assertEquals(0, movie.getRating());
+    }
+
+    @Test
+    void build_shouldCreateTvSeriesWithCorrectParams(){
+        TvSeries tvSeries = new TvSeriesBuilder()
+                .title("Peaky Blinders")
+                .year(2018)
+                .episodes(48)
+                .seasons(4)
+                .build();
+
+        assertEquals("Peaky Blinders", tvSeries.getTitle());
+        assertEquals(2018, tvSeries.getYear());
+        assertNull(tvSeries.getGenre());
+        assertEquals(0, tvSeries.getRating());
+        assertEquals(48, tvSeries.getEpCount());
+        assertEquals(4, tvSeries.getSeasonCount());
+        assertNull(tvSeries.getStatus());
+    }
+
 }
--- src/test/java/it/nicolacosta/movie_app/persistence/MediaMapperTest.java
+++ src/test/java/it/nicolacosta/movie_app/persistence/MediaMapperTest.java
@@ -1,4 +1,66 @@
+package it.nicolacosta.movie_app.persistence;
+
+import it.nicolacosta.movie_app.builder.MovieBuilder;
+import it.nicolacosta.movie_app.model.Media;
+import it.nicolacosta.movie_app.model.Movie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 class MediaMapperTest {
-  
+
+    private MediaMapper mapper;
+
+    @BeforeEach
+    void setUp(){
+        mapper = new MediaMapper();
+    }
+
+    @Test
+    void testResultSetToMovie() throws SQLException{
+
+        // Creiamo un finto ResultSet
+        ResultSet rs = mock(ResultSet.class);
+
+        // TODO definire meglio cosa fa questa parte
+        // Definiamo cosa deve rispondere quando il Mapper chiamerà i suoi metodi.
+        when(rs.getString("type")).thenReturn("Film");
+        when(rs.getInt("id")).thenReturn(1);
+        when(rs.getString("title")).thenReturn("Inception");
+        when(rs.getString("director")).thenReturn("Nolan");
+
+
+        Media result = mapper.resultSetToMedia(rs);
+
+        assertInstanceOf(Movie.class, result);
+        assertEquals("Inception", result.getTitle());
+        assertEquals(0, result.getYear());
+
+    }
+
+    @Test
+    void shouldWriteOnStatement() throws SQLException {
+        // Creiamo un finto PreparedStatement per verificare se il mapper ci scrive sopra
+        PreparedStatement stmt = mock(PreparedStatement.class);
+
+        // Creiamo un oggetto Media REALE con dei dati di prova
+        Media movie = new MovieBuilder()
+                .title("Matrix")
+                .director("Wachowski").build();
+
+        MediaMapper mapper = new MediaMapper();
+        int nextIndex = mapper.completeStatement(movie, stmt);
+
+        // Verifichiamo se il Mapper ha chiamato i metodi del DB.
+        verify(stmt).setString(1, "Matrix");
+        verify(stmt).setString(2, "Wachowski");
+        // Verifichiamo che il metodo ci restituisca l'indice corretto per il parametro successivo
+        assertEquals(10, nextIndex);
+        }
+
 }
